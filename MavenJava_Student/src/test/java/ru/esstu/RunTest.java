ecd3ea96c8cf4707c16fedcf58ed48b5509388a8
Эдуард <148698101+Fl1mas@users.noreply.github.com>
2023-12-16 08:15:00 +0300

    bb

--- MavenJava_Student/src/test/java/ru/esstu/RunTest.java
+++
@@ -1,8 +0,0 @@
-package ru.esstu;
-
-public class RunTest extends StudentListTest {
-    @Override
-    protected StudentList createStudentList() {
-        return new StudentListFile("fdsf");
-    }
-}
