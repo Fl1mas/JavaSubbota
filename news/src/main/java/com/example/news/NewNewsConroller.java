5263e8603298955426d843a8eb24876e4e3a40fe
Fl1mas <eduard.fl1mas@gmail.com>
2023-12-18 01:37:42 +0800

    update

--- news/src/main/java/com/example/news/NewNewsConroller.java
+++ news/src/main/java/com/example/news/NewNewsConroller.java
@@ -1,5 +1,6 @@
 package com.example.news;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.swing.text.html.Option;
 import java.util.ArrayList;
@@ -16,6 +18,7 @@ import java.util.Optional;
 public class NewNewsConroller {
     @Autowired
     private PostRepository postRepository;
+
     @GetMapping("/news")
     public String newsMain(Model model){
         Iterable<Post> posts = postRepository.findAll();
@@ -38,6 +41,10 @@ public class NewNewsConroller {
         ArrayList<Post> res = new ArrayList<>();
         post.ifPresent(res::add);
         model.addAttribute("post", res);
-        return "blog-details";
+        return "newsdetails";
+    }
+    @GetMapping("/news/{id}/edit")
+    public String newsEdit(Model model){
+        return "news-add";
     }
 }
--- news/src/main/java/com/example/news/PostRepository.java
+++ news/src/main/java/com/example/news/PostRepository.java
@@ -4,3 +4,4 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.news.NewNewsConroller;
 public interface PostRepository extends CrudRepository<Post, Long> {
 }
+//public interface PostRepository extends JpaRepository<Post, Long> {}
